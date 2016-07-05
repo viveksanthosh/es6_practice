c3db47df7556cc4c608aac1f1892aec6542836ce
viveksanthosh <viveksanthosh.in@gmail.com>
2016-07-05 20:41:19 +0530

    printing car name

--- practice.js
+++ practice.js
@@ -1,33 +1,14 @@
-class Animal{
-constructor(){
-    console.log('called 1');
-  this.canFly = 'maybe'
-  this.alive = true;
-}
-}
 
-class Bird extends Animal{
-  constructor(){
-    super();
-      console.log('called 2');
-      this.canFly = true;
+class Car {
+  constructor(name){
+  this.name = name;
+  }
+
+  printName(){
+    console.log(this.name);
   }
 }
 
-class Crow extends Bird {
-  constructor(){
-    super();
-      console.log('called 3');
-      this.color = 'black';
-  }
-  set setName(name){
-    this.name = name;
-  }
-  get getName(){
-    return this.name;
-  }
-}
-
-var a = new Crow();
-a.setName = "Kaka"
-console.log(a.getName);
+var a = new Car('volvo');
+console.log(a);
+a.printName();
